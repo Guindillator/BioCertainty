cb46f77408fac151da4c29b8f421b816a7394d95
Mario_P <marprigod@gmail.com>
2019-07-18 20:35:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ setup(name='biocertainty',
     ],
     test_suite='tests',
     install_requires=requirements,
-    data_files=[('biocertainty', ['data/training_set.csv'])],
+    include_package_data = True,
+#     data_files=[('biocertainty', ['data/training_set.csv'])],
     # package_data = {'biocertainty': ['data/training_set.csv']},
     setup_requires=[
         # dependency for `python setup.py test`
