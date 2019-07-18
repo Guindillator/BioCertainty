abe77eb22fa7ca6ffeab54698b5abb6d0121b840
Mario_P <marprigod@gmail.com>
2019-07-18 15:40:17 +0200

    Update certainty.py

--- biocertainty/certainty.py
+++ biocertainty/certainty.py
@@ -23,7 +23,7 @@ def Certainty(statement):
     fin = codecs.open(INPUT_FILE,"r",  encoding='utf8')
     maxlen = 0
     for line in fin:
-        sent = (line.strip().replace("\n", ' '))[0]
+        sent = (line.strip().replace("\n", ' ').split('\t'))[0]
         sent = [x for x in nltk.word_tokenize(sent) if x not in stopwords]
         texts.append(' '.join(sent))
         if len(sent) > maxlen:
