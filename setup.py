878656d58046ab29bc8c60a926506d3f8dbe84ac
Mario_P <marprigod@gmail.com>
2019-07-18 21:20:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,33 +18,5 @@ setup(name='biocertainty',
     author='Mario Prieto',	
     author_email='mario.prieto@upm.es',	
     license='Wilkinson Laboratory',
-    packages=['biocertainty'],
-    package_dir={'biocertainty':
-                 'biocertainty'},	
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7'
-    ],
-    test_suite='tests',
-    install_requires=requirements,
-    include_package_data = True,
-#     data_files=[('biocertainty', ['data/training_set.csv'])],
-    package_data = {'biocertainty': ['data/training_set.csv']},
-    setup_requires=[
-        # dependency for `python setup.py test`
-        'pytest-runner',
-        # dependencies for `python setup.py build_sphinx`
-        'sphinx',
-        'recommonmark'
-    ],
-    tests_require=[
-        'pytest',
-        'pytest-cov',
-        'pycodestyle',
-    ]
+    zip_safe=False
 )
