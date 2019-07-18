bcfe8604504bf4ce346dd89898d2cd27867e02ce
Mario_P <marprigod@gmail.com>
2019-07-18 21:24:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-
-setup(name='biocertainty',	
-    version='0.1.0',	
-    description='Python package that provides the certainty about biomedical statements.',	
-    url='https://github.com/Guindillator/BioCertainty',	
-    author='Mario Prieto',	
-    author_email='mario.prieto@upm.es',	
-    license='Wilkinson Laboratory',
-    zip_safe=False
-)
+setup(name='biocertainty',
+  version='0.1',
+  description='Python package that provides the certainty about biomedical statements.',
+  url='https://github.com/Guindillator/BioCertainty',
+  author='Mario Prieto',
+  author_email='mario.prieto@upm.es',
+  license='Wilkinson Laboratory',
+  packages=['biocertainty'],
+  zip_safe=False)
