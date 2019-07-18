59b81a19ed8d8c50010b274df80efd411d2424f9
Mario_P <marprigod@gmail.com>
2019-07-18 13:28:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,13 +35,6 @@ setup(name='biocertainty',
     ],
     test_suite='tests',
     install_requires=requirements,
-    setup_requires=[
-        # dependency for `python setup.py test`
-        'pytest-runner',
-        # dependencies for `python setup.py build_sphinx`
-        'sphinx',
-        'recommonmark'
-    ],
     tests_require=[
         'pytest',
         'pytest-cov',
