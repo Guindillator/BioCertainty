e44a604fa8b876911918fd27548460c61fa6cd7b
Mario_P <marprigod@gmail.com>
2019-07-18 13:23:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,5 +32,19 @@ setup(name='biocertainty',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7'
+    ],
+    test_suite='tests',
+    install_requires=requirements,
+    setup_requires=[
+        # dependency for `python setup.py test`
+        'pytest-runner',
+        # dependencies for `python setup.py build_sphinx`
+        'sphinx',
+        'recommonmark'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pycodestyle',
     ]
 )
